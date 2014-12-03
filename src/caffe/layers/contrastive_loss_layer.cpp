72958ab2fd96dbac5c2757606a203705f4262859
sanjeev <sanjeev.jagannatharao@gmail.com>
2014-12-03 02:30:55 -0800

    adding a forward_gpu method

--- src/caffe/layers/contrastive_loss_layer.cpp
+++ src/caffe/layers/contrastive_loss_layer.cpp
@@ -128,6 +128,13 @@ void ContrastiveLossLayer<Dtype>::Forward_cpu(
   printf("CLL: value of loss : %f \n", loss);
   (*top)[0]->mutable_cpu_data()[0] = loss;
 }
+
+template <typename Dtype>
+void ContrastiveLossLayer<Dtype>::Forward_gpu(
+    const vector<Blob<Dtype>*>& bottom,
+    vector<Blob<Dtype>*>* top) {
+  Forward_cpu(& bottom,top); 
+}
 /*
 template <typename Dtype>
 void ContrastiveLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
