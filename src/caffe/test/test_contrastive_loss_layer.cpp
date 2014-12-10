22348cea02ca639ba7d008a96234ff3a15918853
sanjeev <sanjeev.jagannatharao@gmail.com>
2014-12-09 23:29:59 -0800

    reducing test size

--- src/caffe/test/test_contrastive_loss_layer.cpp
+++ src/caffe/test/test_contrastive_loss_layer.cpp
@@ -60,9 +60,9 @@ class ContrastiveLossLayerTest : public MultiDeviceTest<TypeParam> {
 
 protected:
   ContrastiveLossLayerTest()
-      : blob_bottom_data_i_(new Blob<Dtype>(10, 10, 1, 1)),
-        blob_bottom_data_j_(new Blob<Dtype>(10, 10, 1, 1)),
-        blob_bottom_y_(new Blob<Dtype>(10, 1, 1, 1)),
+      : blob_bottom_data_i_(new Blob<Dtype>(1, 10, 1, 1)),
+        blob_bottom_data_j_(new Blob<Dtype>(1, 10, 1, 1)),
+        blob_bottom_y_(new Blob<Dtype>(1, 1, 1, 1)),
         blob_top_loss_(new Blob<Dtype>()) {
     // fill the values
     FillerParameter filler_param;
