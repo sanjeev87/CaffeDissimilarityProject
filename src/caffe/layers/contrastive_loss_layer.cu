17826cf50dca9ac2c87a52c7d5e4d959ef2b36e3
sanjeev <sanjeev.jagannatharao@gmail.com>
2014-12-14 01:54:26 -0800

    Fix make errors

--- src/caffe/layers/contrastive_loss_layer.cu
+++ src/caffe/layers/contrastive_loss_layer.cu
@@ -181,15 +181,7 @@ void ContrastiveLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       const Dtype sign = (i == 0) ? 1 : -1;
       const Dtype alpha = sign * top[0]->cpu_diff()[0] /
           static_cast<Dtype>((*bottom)[0]->num());
-      // NOLINT_NEXT_LINE(whitespace/operators)
-     /* 
-     CLLForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
-          count, channels, margin, alpha,
-          (*bottom)[2]->gpu_data(),  // pair similarity 0 or 1
-          diff_.gpu_data(),  // the cached eltwise difference between a and b
-          dist_sq_.gpu_data(),  // the cached square distance between a and b
-          (*bottom)[i]->mutable_gpu_diff());
-      */
+
       int num = (*bottom)[i]->num();
 
     for (int j = 0; j < num; ++j) {
@@ -200,14 +192,6 @@ void ContrastiveLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
             bout[(j*channels) + k] += sign * dist_sq_.mutable_cpu_data()[j] 
                                     * gradient_sign * 4 / margin;
           }
-          /*
-          caffe_cpu_axpby(
-              channels,
-              alpha,
-              diff_.cpu_data() + (j*channels),
-              Dtype(0.0),
-              bout + (j*channels));
-          */
         } else {  // dissimilar pairs
           
           for(int k = 0 ; k < channels ; k ++){
