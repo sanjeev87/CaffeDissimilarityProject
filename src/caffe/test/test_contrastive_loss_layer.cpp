45bc9e19412c129bc737e94e96e8212046ebacfc
sanjeev <sanjeev.jagannatharao@gmail.com>
2014-12-13 23:53:24 -0800

    Adding some stuff for testing ... Should be removed git push origin master after testing git push origin master

--- src/caffe/test/test_contrastive_loss_layer.cpp
+++ src/caffe/test/test_contrastive_loss_layer.cpp
@@ -74,7 +74,9 @@ protected:
     filler.Fill(this->blob_bottom_data_j_);
     blob_bottom_vec_.push_back(blob_bottom_data_j_);
     for (int i = 0; i < blob_bottom_y_->count(); ++i) {
-      blob_bottom_y_->mutable_cpu_data()[i] = caffe_rng_rand() % 2;  // 0 or 1
+      //TODO:Remove
+      blob_bottom_y_->mutable_cpu_data()[i] = 0;  // 0 or 1
+      //blob_bottom_y_->mutable_cpu_data()[i] = caffe_rng_rand() % 2;  // 0 or 1
     }
     blob_bottom_vec_.push_back(blob_bottom_y_);
     blob_top_vec_.push_back(blob_top_loss_);
